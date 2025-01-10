30fbeff1973ed6a8b34286bdf714a921c361b7f5
aymenthedeveloper <aymendev.contact@gmail.com>
2025-01-10 16:53:42 +0100

    added persist dark mode functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,19 @@ import Header from "./components/Header"
 import Main from "./components/Main";
 
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const isDarkMode = (JSON.parse(localStorage.getItem('dark-mode')) || {}).darkMode
+  const [darkMode, setDarkMode] = useState(isDarkMode || false);
+  function persiseData(){
+    localStorage.setItem('dark-mode', JSON.stringify({darkMode}))
+  }
   useEffect(()=>{
-    if (darkMode) document.body.classList.add('dark')
-    else document.body.classList.remove('dark')
+    if (darkMode) {
+      document.body.classList.add('dark')
+    }
+    else {
+      document.body.classList.remove('dark')
+    }
+    persiseData()
   }, [darkMode])
   return (
     <div className="wrapper">
