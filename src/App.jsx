b0b3c9c08a7a407f32a68be23c10b37de9e14633
aymenthedeveloper <aymendev.contact@gmail.com>
2025-01-14 22:26:34 +0100

    moved dark mode logic to header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,11 @@
-import { useEffect, useState } from "react"
+
 import Header from "./components/Header"
 import Main from "./components/Main";
 
 function App() {
-  const isDarkMode = (JSON.parse(localStorage.getItem('dark-mode')) || {}).darkMode
-  const [darkMode, setDarkMode] = useState(isDarkMode || false);
-  function persiseData(){
-    localStorage.setItem('dark-mode', JSON.stringify({darkMode}))
-  }
-  useEffect(()=>{
-    if (darkMode) {
-      document.body.classList.add('dark')
-    }
-    else {
-      document.body.classList.remove('dark')
-    }
-    persiseData()
-  }, [darkMode])
   return (
     <div className="wrapper">
-      <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+      <Header/>
       <Main />
     </div>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { useEffect, useState } from "react"
 import moonOutline from "../assets/moon-outline.png"
 import moonFill from "../assets/moon-fill.png"
 
-export default function Header(props) {
-  const {darkMode, setDarkMode} = props;
+export default function Header() {
+  const isDarkMode = (JSON.parse(localStorage.getItem('dark-mode')) || {value: false})
+  const [darkMode, setDarkMode] = useState(isDarkMode.value);
+  function persisetData(){
+    localStorage.setItem('dark-mode', JSON.stringify({value: darkMode}))
+  }
+  useEffect(()=>{
+    if (darkMode) {
+      document.body.classList.add('dark')
+    }
+    else {
+      document.body.classList.remove('dark')
+    }
+    persisetData()
+  }, [darkMode])
   return (
     <header>
       <div className="logo">Where in the world?</div>
